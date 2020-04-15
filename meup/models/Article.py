50047b01c7216fb6403533d1e2bc8fb0c9bae151
abheist <abhishek291093@gmail.com>
2020-04-15 21:01:16 +0530

    Article.py, added published field

--- meup/models/Article.py
+++ meup/models/Article.py
@@ -7,6 +7,7 @@ class Article(models.Model):
     title = models.CharField(blank=False, null=False, max_length=255)
     author = models.ForeignKey(to=get_user_model(), on_delete=models.CASCADE)
     content = models.TextField()
+    published = models.BooleanField(blank=False, default=False)
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
