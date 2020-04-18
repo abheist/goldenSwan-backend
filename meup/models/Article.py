0c77460aa20868c2c019fba49c826c92a55c6f25
abheist <abhishek291093@gmail.com>
2020-04-18 22:50:58 +0530

    🧞‍ article , added in the api

--- meup/models/Article.py
+++ meup/models/Article.py
@@ -6,8 +6,8 @@ from django.db import models
 
 class Article(models.Model):
     slug = models.SlugField(max_length=120, blank=False, null=False, unique=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(blank=False, null=False, max_length=100)
-    subtitle = models.CharField(max_length=140, blank=True)
+    title = models.CharField(blank=True, null=True, max_length=100)
+    subtitle = models.CharField(max_length=140, blank=True, null=True)
     author = models.ForeignKey(to=get_user_model(), on_delete=models.CASCADE)
     content = models.TextField()
     published = models.BooleanField(blank=False, default=False)
