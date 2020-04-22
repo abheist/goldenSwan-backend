ab70e17ba0a5ed1cc21041c1e5c35b82d4adec58
abheist <abhishek291093@gmail.com>
2020-04-22 20:46:00 +0530

    🧞‍ users, change in model, added bio, dob and profile fields

--- users/models/user.py
+++ users/models/user.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    bio = models.CharField(max_length=500, blank=True)
+    dob = models.DateField(null=True, blank=True)
+    profilePic = models.ImageField(upload_to='profiles/', blank=True, null=True)
