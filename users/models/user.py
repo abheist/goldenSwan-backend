b491612f4a4eb4445cc098b4470895ad43f2e838
abheist <abhishek291093@gmail.com>
2020-05-16 18:15:50 +0530

    🚧 updated user model to include the social profiles of the users

--- users/models/user.py
+++ users/models/user.py
@@ -6,3 +6,7 @@ class User(AbstractUser):
     bio = models.CharField(max_length=500, blank=True)
     dob = models.DateField(null=True, blank=True)
     profile_pic = models.ImageField(upload_to='profiles/', blank=True, null=True)
+    facebook = models.CharField(max_length=128, blank=True)
+    twitter = models.CharField(max_length=128, blank=True)
+    instagram = models.CharField(max_length=128, blank=True)
+    linkedin = models.CharField(max_length=128, blank=True)
