6bd4c5f2b9003f3022f004d028577ee96881f927
abheist <abhishek291093@gmail.com>
2020-05-17 17:42:28 +0530

    ♻️ model profile_pic to text field to accept it as a jsons content from cloduinary

--- users/models/user.py
+++ users/models/user.py
@@ -5,7 +5,7 @@ from django.db import models
 class User(AbstractUser):
     bio = models.CharField(max_length=500, blank=True)
     dob = models.DateField(null=True, blank=True)
-    profile_pic = models.ImageField(upload_to='profiles/', blank=True, null=True)
+    profile_pic = models.TextField(null=True, blank=True)
     facebook = models.CharField(max_length=128, blank=True)
     twitter = models.CharField(max_length=128, blank=True)
     instagram = models.CharField(max_length=128, blank=True)
